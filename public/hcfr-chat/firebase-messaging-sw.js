532be1eb2adfbee98837a675ae4d1394c30831d6
Chris Johnson <binbash0597@gmail.com>
2020-11-11 12:39:10 -0500

    cleaned up files

---
+++ public/hcfr-chat/firebase-messaging-sw.js
@@ -0,0 +1,5 @@
+importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-messaging.js');
+importScripts('../scripts/init-firebase.js');
+
+firebase.messaging();
