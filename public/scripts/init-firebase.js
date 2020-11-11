a115ed7ecf79634723c0a49f1bf8a9a0afe5f083
Chris Johnson <binbash0597@gmail.com>
2020-11-11 05:47:53 -0500

    added files

---
+++ public/scripts/init-firebase.js
@@ -0,0 +1,13 @@
+  var firebaseConfig = {
+    apiKey: "AIzaSyB1QQEPJCsLkTRkYG-et5nq4m6Z-1ZSqNA",
+    authDomain: "hcfr-mobile-guide.firebaseapp.com",
+    databaseURL: "https://hcfr-mobile-guide.firebaseio.com",
+    projectId: "hcfr-mobile-guide",
+    storageBucket: "hcfr-mobile-guide.appspot.com",
+    messagingSenderId: "77881030995",
+    appId: "1:77881030995:web:2fb706ab0ae5fafac889a6",
+    measurementId: "G-611H8SYE47"
+  };
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
